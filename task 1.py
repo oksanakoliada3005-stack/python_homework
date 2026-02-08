e156cf7c3f2de84f37e528f557333d13581c295c
Oksana <oksanakoliada3005@gmail.com>
2026-02-08 23:18:10 +0200

    first commit

---
+++ task 1.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+def get_days_from_today(date):
+    try:
+        # перетворюємо рядок у дату
+        given_date = datetime.strptime(date, "%Y-%m-%d").date()
+
+        # поточна дата (без часу)
+        today = datetime.today().date()
+
+        # різниця
+        delta = today - given_date
+
+        return delta.days
+
+    except ValueError:
+        print("Неправильний формат дати. Використовуйте YYYY-MM-DD")
+        return None
+
+# приклад перевірки
+print(get_days_from_today("2020-10-09"))
