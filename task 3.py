7ee83aa4a91a19187eaf2e8549888de3dafcfe29
Oksana <oksanakoliada3005@gmail.com>
2026-02-08 23:35:31 +0200

    task3 done

---
+++ task 3.py
@@ -0,0 +1,35 @@
+import re
+
+def normalize_phone(phone_number):
+    #видаляємо всі символи, крім цифр та +
+    phone = re.sub(r"[^\d+]", "", phone_number)
+
+    # якщо номер починається з +
+    if phone.startswith("+"):
+        digits = phone[1:]
+    else:
+        digits = phone
+
+    #якщо номер починається з 380 → додаємо +
+    if digits.startswith("380"):
+        return "+" + digits
+    
+    #якщо номер починається без коду країни → додаємо +38
+    return "+38" + digits
+
+
+#перевірка
+raw_numbers = [
+    "067\t123 4567",
+    "(095) 234-5678\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+print("Нормалізовані номери:", sanitized_numbers)
